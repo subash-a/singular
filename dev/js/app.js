702694d5291beecadaaa77fa67a1a8e50755d38f
Subhash A <s7subash@gmail.com>
2014-08-12 21:33:29 +0530

    added gallery creation
    Added new images
    Added styling css files for bootstrap
    Added jquery for bootstrap support
    Added new angular module for separation of code
    Added html template for directive

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,19 +1,4 @@
 (function () {
-    var app = angular.module("viewer",[]);
-    app.controller("imageBlock", function () {
-	this.block = imageblock;
-    });
-    var imageblock = {
-	"title": "Kangaroo Jack",
-	"source": "assets/images/buck.jpg",
-	"description": "The coolest Kangaroo in the world!",
-	"comments": [{"body": "Superlike!!!","author":"superBall"}]
-    };
-    app.controller("thumbnailBlock", function () {
-	this.list = thumbnailblock;
-	this.thumbSelect = function () {
-	    alert("Thumbnail clicked");
-	};
-    });
-    var thumbnailblock = {"thumbs":[{"source":"assets/images/buck.jpg"}]};
+
+    var app = angular.module("PhotoBook",["ImageGalleryModule"]);
 } ());
---
+++ dev/js/imageGallery.js
@@ -0,0 +1,43 @@
+(function () {
+    var images = [{
+	"title": "Kangaroo Jack",
+	"source": "assets/images/buck.jpg",
+	"thumbnail":{"source": "assets/images/buck.jpg"},
+	"description": "The coolest Kangaroo in the world!",
+	"comments": [{"body": "Superlike!!!","author":"superBall"}]
+    },{
+	"title": "Tiger Stand",
+	"source": "assets/images/tiger.jpg",
+	"thumbnail":{"source": "assets/images/tiger.jpg"},
+	"description": "The coolest Tiger in the world!",
+	"comments": [{"body": "Superlike!!!","author":"superBall"}]
+    },{
+	"title": "Bengal Tiger",
+	"source": "assets/images/bengal.jpg",
+	"thumbnail":{"source": "assets/images/bengal.jpg"},
+	"description": "The coolest Bengal Tiger in the world!",
+	"comments": [{"body": "Superlike!!!","author":"superBall"}]
+    },{
+	"title": "Eye of the Tiger",
+	"source": "assets/images/eyes.jpg",
+	"thumbnail":{"source": "assets/images/eyes.jpg"},
+	"description": "The coolest Eyes in the world!",
+	"comments": [{"body": "Superlike!!!","author":"superBall"}]
+    }];
+
+    var app = angular.module("ImageGalleryModule", []);
+    app.directive ("imageGallery", function () {
+	return {
+	    "restrict": "E",
+	    "templateUrl": "js/imageGallery.html",
+	    "controller": function () {
+		this.images = images;
+		this.currentImage = images[0];
+		this.changeImage = function (image) {
+		    this.currentImage = image;
+		};
+	    },
+	    "controllerAs": "gallery"
+	}
+    });  
+} ());
